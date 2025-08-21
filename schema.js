a77c835f3b3edbb82780d79822b1745b76e81d67
Sumit Kasbe <sumitkasbe8382@gmail.com>
2025-08-21 16:50:20 +0530

    update schema.js

--- schema.js
+++ schema.js
@@ -7,7 +7,10 @@ module.exports.listingSchema = Joi.object({
         location: Joi.string().required(),
         country: Joi.string().required(),
         price: Joi.number().required().min(0),
-        image: Joi.string().allow('', null),
+        image: Joi.object({
+            url: Joi.string().uri().required(),
+            filename: Joi.string().allow('', null)
+        }).required(),
         category: Joi.string().valid(
             "Trending",
             "Rooms",
