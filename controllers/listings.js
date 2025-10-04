88b679899b9712a353453066f18188b4d72d0888
sumitkasbe <sumitkasbe8382@gmail.com>
2025-10-04 13:49:25 +0530

    Fixixng image error

--- controllers/listings.js
+++ controllers/listings.js
@@ -26,16 +26,26 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
-    let url = req.file.path;
-    let filename = req.file.filename;
-    const newListing = new Listing(req.body.listing);
-    newListing.owner = req.user._id;
-    newListing.image = { url, filename };
-    await newListing.save();
+    try {
+        const newListing = new Listing(req.body.listing);
+        newListing.owner = req.user._id;
 
-    req.flash("success", "New Listing Created!");
-    res.redirect("/listings");
+        if (!req.file) {
+            req.flash("error", "Please upload an image!");
+            return res.redirect("/listings/new");
+        }
+
+        const { path: url, filename } = req.file;
+        newListing.image = { url, filename };
+        await newListing.save();
+
+        req.flash("success", "New Listing Created!");
+        res.redirect(`/listings/${newListing._id}`);
+    } catch (err) {
+        next(err);
+    }
 };
+
 
 module.exports.renderEditForm = async (req, res) => {
     let { id } = req.params;
