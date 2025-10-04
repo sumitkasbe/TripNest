64435bf7127350f88fc2739adf3c43800f1c51d6
sumitkasbe <sumitkasbe8382@gmail.com>
2025-10-05 01:04:52 +0530

    Fixixng update image error

--- schema.js
+++ schema.js
@@ -1,17 +1,16 @@
 const Joi = require('joi');
-const review = require('./models/review');
 
 module.exports.listingSchema = Joi.object({
-    listing : Joi.object({
+    listing: Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
         location: Joi.string().required(),
         country: Joi.string().required(),
         price: Joi.number().required().min(0),
         image: Joi.object({
-            url: Joi.string().uri().required(),
-            filename: Joi.string().allow('').optional()
-        }).required(),
+            url: Joi.string().uri().allow(''),     // can be empty string if no new image
+            filename: Joi.string().allow('')       // can be empty string if no new image
+        }).optional(),                              // image object itself is optional
         category: Joi.string().valid(
             "Trending",
             "Rooms",
@@ -23,14 +22,7 @@ module.exports.listingSchema = Joi.object({
             "Farms",
             "Arctic",
             "Domes",
-            "Boats",
-        ).required(),
-    }).required()
-});
-
-module.exports.reviewSchema = Joi.object({
-    review:Joi.object({
-        rating:Joi.number().required().min(1).max(5),
-        comment:Joi.string().required(),
+            "Boats"
+        ).required()
     }).required()
 });
