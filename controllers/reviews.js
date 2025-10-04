15b598d10021275f74718a55ff292c82ead00cf0
sumitkasbe <sumitkasbe8382@gmail.com>
2025-10-04 16:40:27 +0530

    Fixixng review error

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,18 +1,34 @@
 const Listing = require("../models/listing.js");
 const Review = require("../models/review");
 
-module.exports.createReview = async (req, res, next) => {
-    let { id } = req.params;
-    let listing = await Listing.findById(req.params.id);
+// module.exports.createReview = async (req, res, next) => {
+//     let { id } = req.params;
+//     let listing = await Listing.findById(req.params.id);
+//     let newReview = new Review(req.body.review);
+//     newReview.author = req.user._id;// review with associative author...
+//     listing.reviews.push(newReview);
+//     console.log(newReview);
+
+//     await newReview.save();
+//     await listing.save();
+//      req.flash("success", "New Review Created!"); 
+//     res.redirect(`/listings/${id}`);
+// };
+
+module.exports.createReview = async(req,res) => {
+    let listings = await Listing.findById(req.params.id);
+    if (!listings) {
+        throw new ExpressError(404, "Listing not found");
+    }
     let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;// review with associative author...
-    listing.reviews.push(newReview);
-    console.log(newReview);
+    newReview.author = req.user._id;
+    listings.reviews.push(newReview);
 
     await newReview.save();
-    await listing.save();
-     req.flash("success", "New Review Created!"); 
-    res.redirect(`/listings/${id}`);
+    await listings.save();
+    req.flash('success','Review submitted successfully!');
+
+    res.redirect(`/listings/${listings._id}`);
 };
 
 module.exports.destroyReview = async (req, res) => {
