247be028f2a2900f17ee257499b95b53a5a747ea
sumitkasbe <sumitkasbe8382@gmail.com>
2025-10-05 00:06:01 +0530

    Fixixng update image error

--- controllers/listings.js
+++ controllers/listings.js
@@ -60,8 +60,7 @@ module.exports.updateListing = async (req, res) => {
         listing.image = { url, filename };
         await listing.save();
     }
-
-    req.flash("success", "Listing Updated!");
+    req.flash('success', 'Listing updated successfully!');
     res.redirect(`/listings/${id}`);
 };
 
